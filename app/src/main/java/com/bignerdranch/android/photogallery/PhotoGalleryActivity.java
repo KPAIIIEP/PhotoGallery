92f8400b0b95e5335adda5751376e65aaacc99d4
Crush <art60@rambler.ru>
2020-11-03 23:14:03 +0300

    Код главы 25

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
@@ -1,14 +1,11 @@
 package com.bignerdranch.android.photogallery;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import android.os.Bundle;
-
-public class PhotoGalleryActivity extends AppCompatActivity {
+public class PhotoGalleryActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
     }
 }
