180ef5cd70bae608d7e9fc37c9c024e3d2054952
Crush <art60@rambler.ru>
2020-11-13 10:59:45 +0300

    Упражнение главы 30. Использование кнопки Back для работы с историей просмотра

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageActivity.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageActivity.java
@@ -3,10 +3,13 @@ package com.bignerdranch.android.photogallery;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.webkit.WebView;
 
 import androidx.fragment.app.Fragment;
 
 public class PhotoPageActivity extends SingleFragmentActivity {
+    WebView mWebView;
+
     public static Intent newIntent(Context context, Uri photoPageUri) {
         Intent i = new Intent(context, PhotoPageActivity.class);
         i.setData(photoPageUri);
@@ -16,5 +19,15 @@ public class PhotoPageActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return PhotoPageFragment.newInstance(getIntent().getData());
+    }
+
+    @Override
+    public void onBackPressed() {
+        mWebView = findViewById(R.id.web_view);
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
